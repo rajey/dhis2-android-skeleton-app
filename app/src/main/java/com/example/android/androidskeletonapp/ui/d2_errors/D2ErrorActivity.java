22622be914d0111d3ac3fcc001b22e0372a430ed
Rajab Mkomwa <rajeyw@gmail.com>
2019-07-04 16:06:57 +0300

    add support to show all avaialable errors

--- app/src/main/java/com/example/android/androidskeletonapp/ui/d2_errors/D2ErrorActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/d2_errors/D2ErrorActivity.java
@@ -1,9 +1,22 @@
 package com.example.android.androidskeletonapp.ui.d2_errors;
 
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.lifecycle.LiveData;
+import androidx.paging.PagedList;
 
 import com.example.android.androidskeletonapp.R;
+import com.example.android.androidskeletonapp.data.Sdk;
 import com.example.android.androidskeletonapp.ui.base.ListActivity;
+
+import org.hisp.dhis.android.core.maintenance.D2Error;
+
+import java.util.List;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class D2ErrorActivity extends ListActivity {
 
@@ -11,6 +24,25 @@ public class D2ErrorActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setUp(R.layout.activity_d2_errors, R.id.d2ErrorsToolbar, R.id.d2ErrorsRecyclerView);
-        // TODO List D2 errors
+        observeErrors();
+
     }
+
+
+    private void observeErrors() {
+        D2ErrorAdapter errorAdapter = new D2ErrorAdapter();
+
+        recyclerView.setAdapter(errorAdapter);
+
+        LiveData<PagedList<D2Error>> errorsPageListLiveData = Sdk.d2().maintenanceModule().d2Errors.getPaged(10);
+
+        errorsPageListLiveData.observe(this, d2ErrorList -> {
+            errorAdapter.submitList(d2ErrorList);
+            findViewById(R.id.d2ErrorsNotificator).setVisibility(d2ErrorList.isEmpty() ? View.VISIBLE : View.GONE);
+        });
+
+
+
+    }
+
 }
--- app/src/main/java/com/example/android/androidskeletonapp/ui/main/MainActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/main/MainActivity.java
@@ -10,6 +10,7 @@ import com.example.android.androidskeletonapp.R;
 import com.example.android.androidskeletonapp.data.Sdk;
 import com.example.android.androidskeletonapp.data.service.ActivityStarter;
 import com.example.android.androidskeletonapp.data.service.SyncStatusHelper;
+import com.example.android.androidskeletonapp.ui.d2_errors.D2ErrorActivity;
 import com.example.android.androidskeletonapp.ui.programs.ProgramsActivity;
 import com.example.android.androidskeletonapp.ui.tracked_entity_instances.TrackedEntityInstancesActivity;
 import com.example.android.androidskeletonapp.ui.tracked_entity_instances.search.TrackedEntityInstanceSearchActivity;
@@ -256,10 +257,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
-
-        // TODO Launch d2 errors activity
-
-        if (id == R.id.navPrograms) {
+        if (id == R.id.navD2Errors) {
+            ActivityStarter.startActivity(this, D2ErrorActivity.class, false);
+        } else if (id == R.id.navPrograms) {
             ActivityStarter.startActivity(this, ProgramsActivity.class,false);
         } else if (id == R.id.navTrackedEntities) {
             ActivityStarter.startActivity(this, TrackedEntityInstancesActivity.class,false);
